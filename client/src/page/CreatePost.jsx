7b942815620d7b932ff40b2415271c631e53c597
ranjan-dev40 <roy984258@gmail.com>
2023-07-14 02:14:59 +0530

    modify

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -30,7 +30,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://image-generation-ai-jw7p.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -60,7 +60,7 @@ const CreatePost = () => {
 
       try {
 
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://image-generation-ai-jw7p.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
